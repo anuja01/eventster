582d19dbe34484837bd90882d1216009b81b6e31
Anuja Ulpathakumbura <anuja.ulp@gmail.com>
2022-05-15 09:46:26 +0200

    initial commit foodie-feed

--- device/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once("../config.php");
-
-$id = $_GET['id'];
-
-$result = mysqli_query($mysqli, "DELETE FROM device WHERE device_id=$id");
-
-header("Location:../home.php");
-?>
--- employee/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once("../config.php");
-
-$id = $_GET['id'];
-
-$result = mysqli_query($mysqli, "DELETE FROM employee WHERE emp_id=$id");
-
-header("Location:../home.php");
-?>
